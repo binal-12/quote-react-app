cb5ba80296f9ec1cb176f8280afca5227d775766
binal-12 <binalchhodvadiya12@gmail.com>
2022-01-26 17:10:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class Appnew extends React.Component{
+
+    state = {
+        text: '',
+        author: ''
+    }
+
+    componentDidMount() {
+        this.fetchQuote()
+    }
+
+    fetchQuote = () => {
+        const rand = Math.floor(Math.random() * 1643)
+        axios.get('https://type.fit/api/quotes')
+            .then((response) => {
+                const { text, author } = response.data[rand]
+                this.setState({ text , author})
+            })
+    }
+
+    render(){
+        const {text, author} = this.state
+        return(
+            <div className="app">
+                <div className="container">
+                    <h1>{text}</h1>
+                    <h2>- {author}</h2>
+                    <button className="btn" onClick={this.fetchQuote}>
+                        <span>Next Quote</span>
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default Appnew
